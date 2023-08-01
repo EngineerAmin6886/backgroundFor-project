e658b8beb6159974ce88cf7855b7aaa1e06d838a
EngineerAmin6886 <127695649+EngineerAmin6886@users.noreply.github.com>
2023-08-01 19:29:10 +0330

    project
    tamil

---
+++ app.js
@@ -0,0 +1,9 @@
+// backdroundFor-project
+// gr:amin
+// 1402-05-10
+let rang = document.getElementsByClassName('amin');
+for(let i = 0; i < rang.length ; i++){
+    rang[0].style.backgroundColor = 'green';
+    rang[1].style.backgroundColor = 'white';
+    rang[2].style.backgroundColor = 'red';
+}
